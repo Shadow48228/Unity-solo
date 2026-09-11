808f3bf6d99eced86197119de35fd3bad445731e
Shadow48228 <justusv911@gmail.com>
2026-09-11 10:24:57 -0700

    start code

--- FPS game/Assets/scripts/Player Controller.cs
+++ FPS game/Assets/scripts/Player Controller.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+    public float jumpHeight = 10f;
+
+    PlayerInput playerInput;
+    Rigidbody rb;
+
+    Vector2 moveInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+
+        moveInput = new Vector2();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 tempMovement = rb. linearVelocity;
+
+        rb.linearVelocity = (moveInput.x * speed) + (moveInput.y * speed) + (moveInput.z * speed);
     }
+
+    // For specific movement
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+     
+    }
+
 }
