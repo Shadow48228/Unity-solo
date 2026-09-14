f8fac0127634314138d3d2007552d6c3affa10d0
Shadow48228 <justusv911@gmail.com>
2026-09-14 09:59:40 -0700

    movement

--- FPS game/Assets/scripts/Player Controller.cs
+++ FPS game/Assets/scripts/Player Controller.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
+
 {
     public float speed = 5.0f;
-    public float jumpHeight = 10f;
+    public float jumpHeight = 2f;
+    public float jumpDetectDistance = 1.1f;
 
     PlayerInput playerInput;
     Rigidbody rb;
 
+    Ray jumpRay;
     Vector2 moveInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,22 +20,39 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         playerInput = GetComponent<PlayerInput>();
 
-        moveInput = new Vector2();
+        moveInput = Vector2.zero;
+
+        jumpRay = new Ray(transform.position, -transform.up);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 tempMovement = rb. linearVelocity;
+        jumpRay.origin = transform.position;
+        jumpRay.direction = -transform.up;
 
-        rb.linearVelocity = (moveInput.x * speed) + (moveInput.y * speed) + (moveInput.z * speed);
+        Vector3 tempMove = rb.linearVelocity;
+
+        tempMove.x = (moveInput.x * speed) * transform.right.x;
+        tempMove.z = (moveInput.y * speed) * transform.forward.z;
+
+        rb.linearVelocity = tempMove;
+
     }
 
     // For specific movement
     public void Move(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
-     
+
+    }
+
+    public void Jump()
+    {
+        if (Physics.Raycast(jumpRay, jumpDetectDistance)) ;
+        {
+            rb.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
+        }
     }
 
 }
